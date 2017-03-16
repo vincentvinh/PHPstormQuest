e5e96dd18f15ad3d82183d1499cedb74f5c168f9
Christophe_vinc <vincentchristophe177@gmail.com>
2017-03-16 10:33:25 +0100

    Initial commit

---
+++ phpstormQ.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wcs-fontainebleau
+ * Date: 16/03/17
+ * Time: 10:27
+ */
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
